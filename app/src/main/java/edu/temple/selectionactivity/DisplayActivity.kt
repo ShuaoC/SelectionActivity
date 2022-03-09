6a3a0746212b2b017d20bea332e4c5dbe1448eb1
ShuaoC <felix19981228@gmail.com>
2022-03-09 13:49:12 -0500

    image can now display in new activity

--- app/src/main/java/edu/temple/selectionactivity/DisplayActivity.kt
+++ app/src/main/java/edu/temple/selectionactivity/DisplayActivity.kt
@@ -1,6 +1,7 @@
 package edu.temple.selectionactivity
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -14,6 +15,12 @@ class DisplayActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display)
 
+        val image = findViewById<ImageView>(R.id.image)
+        val name = findViewById<TextView>(R.id.title)
+        val des = findViewById<TextView>(R.id.desription)
+        val button = findViewById<Button>(R.id.button)
+        val obj = intent.extras?.get("games") as ImageObject
 
+        image.setBackgroundResource(obj.drawable)
     }
 }
